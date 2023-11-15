2435be2225d6cd97bf8cdb5c7185edfdecaa6d5c
prakash-d07 <saiprakash2488@gmail.com>
2023-11-14 22:57:45 -0500

    readme updated

--- src/Flight_Fare_estimator_Project/utils/common.py
+++ src/Flight_Fare_estimator_Project/utils/common.py
@@ -36,20 +36,20 @@ def read_yaml(path_to_yaml:Path) ->ConfigBox:
 
 @ensure_annotations
 def create_directories(path_to_directories:list,verbose=True):
-        """create list of directories
+    """create list of directories
 
     Args:
         path_to_directories (list): list of path of directories
         ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
     for path in path_to_directories:
-        os.makedirs(path,exist_ok=True)
-        if verbose: 
-            logger.info(f"Created Director at {path}")
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
 
 @ensure_annotations
 def save_json(path:Path,data=dict):
-        """save json data
+    """save json data
 
     Args:
         path (Path): path to json file
@@ -92,7 +92,7 @@ def load_json(path: Path) -> ConfigBox:
 
 
 @ensure_annotations
-def save_bin(data=Any,path:Path):
+def save_bin(data: Any,path:Path):
     """save binary file
 
     Args:
